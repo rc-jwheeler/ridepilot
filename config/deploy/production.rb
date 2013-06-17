54a723e322a2cc0be83ffa8bb0a6fe714344124d
Mike Leone <michael.leone@panopticdev.com>
2013-06-17 00:22:47 -0400

    Explicitly set rails envs in deploy scripts.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,7 @@
 set :deploy_to, "/home/deployer/rails/ridepilot"
 set :branch, "stable"
 set :rvm_ruby_string, '1.9.2'
+set :rails_env, "production"
 
 role :web, "184.154.79.122"
 role :app, "184.154.79.122"
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,7 @@
 set :deploy_to, "/home/deployer/rails/ridepilot"
 set :branch, "master"
 set :rvm_ruby_string, '1.9.2'
+set :rails_env, "staging"
 
 role :web,  "184.154.158.74"
 role :app,  "184.154.158.74"
