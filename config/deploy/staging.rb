ead5cdfb79610c0bc7e31411e210acfbd1b2b093
Mike Leone <michael.leone@panopticdev.com>
2013-06-17 00:19:48 -0400

    Add new staging server URL to config.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,7 @@
-set :deploy_to, "/home/deployer/rails/ridepilot-staging"
+set :deploy_to, "/home/deployer/rails/ridepilot"
 set :branch, "master"
 set :rvm_ruby_string, '1.9.2'
-role :web, "184.154.79.122"
-role :app, "184.154.79.122"
-role :db,  "184.154.79.122", :primary => true # This is where Rails migrations will run
+
+role :web,  "184.154.158.74"
+role :app,  "184.154.158.74"
+role :db,   "184.154.158.74", :primary => true # This is where Rails migrations will run
