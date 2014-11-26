d92d4e020228cd05b4c42e87b31d88e69b7e42d7
Chris Bloom <chrisbloom7@gmail.com>
2014-11-26 11:10:19 -0500

    escape_utils initializer updates

--- config/initializers/escape_utils.rb
+++ config/initializers/escape_utils.rb
@@ -1,7 +1,3 @@
-module Rack
-  module Utils
-    def escape(s)
-      EscapeUtils.escape_url(s)
-    end
-  end
-end
+require 'escape_utils/html/rack' # to patch Rack::Utils
+require 'escape_utils/html/erb' # to patch ERB::Util
+require 'escape_utils/html/cgi' # to patch CGI
