4a257799e8268a8e0e35c426c82b424742dae598
Chris Bloom <chrisbloom7@gmail.com>
2015-03-18 12:38:30 -0400

    Fixing bad statement in migration file

--- db/migrate/20120810192249_remove_rememberable_user_password.rb
+++ db/migrate/20120810192249_remove_rememberable_user_password.rb
@@ -1,11 +1,9 @@
 class RemoveRememberableUserPassword < ActiveRecord::Migration
   def self.up
-    remove_column :users, :remember_token
     remove_column :users, :remember_created_at
   end
  
   def self.down
     add_column :users, :remember_created_at, :datetime
-    add_column :users, :remember_token, :string
   end
 end
