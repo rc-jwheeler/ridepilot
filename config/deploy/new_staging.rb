063008e10d6d421cd01030cc70408f5af0d7c965
Chris Bloom <chrisbloom7@gmail.com>
2015-03-19 00:59:06 -0400

    Make sure Capistrano knows about RAILS_RELATIVE_URL_ROOT when it compiles assets

--- config/deploy/new_staging.rb
+++ config/deploy/new_staging.rb
@@ -2,4 +2,5 @@ set :branch, 'rails-upgrade'
 set :rvm_ruby_version, '2.2.0@ridepilot'
 set :deploy_to, '/home/deploy/rails/ridepilot'
 set :rails_env, 'staging'
+set :default_env, { "RAILS_RELATIVE_URL_ROOT" => "/ridepilot" }
 server 'apps2.rideconnection.org', roles: [:app, :web, :db], user: 'deploy'
