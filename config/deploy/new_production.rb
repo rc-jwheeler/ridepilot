54ce1cdf84239c70110b660bf206a6906d5920cc
Chris Bloom <chrisbloom7@gmail.com>
2015-05-01 11:47:17 -0400

    Bumping ruby version dependency to 2.2.1 (2.2.0 was revoked due to security issue)

--- config/deploy/new_production.rb
+++ config/deploy/new_production.rb
@@ -1,5 +1,5 @@
 set :branch, 'rails-upgrade'
-set :rvm_ruby_version, '2.2.0@ridepilot'
+set :rvm_ruby_version, '2.2.1@ridepilot'
 set :deploy_to, '/home/deploy/rails/ridepilot'
 set :rails_env, 'production'
 set :default_env, { "RAILS_RELATIVE_URL_ROOT" => "/ridepilot" }
