e32eccf3edb19d53e53e6912b5f5e1928471e1a0
Xudong Liu <xudong.camsys@gmail.com>
2015-06-01 14:33:43 -0400

    ensure app starts/loads fine

--- config/initializers/01_load_app_config.rb
+++ config/initializers/01_load_app_config.rb
@@ -1,2 +1,7 @@
-raw_config = File.read("#{Rails.root}/config/app_config.yml")
-APP_CONFIG = YAML.load(raw_config)[Rails.env].symbolize_keys
+APP_CONFIG = {}
+begin
+  raw_config = File.read("#{Rails.root}/config/app_config.yml")
+  APP_CONFIG = YAML.load(raw_config)[Rails.env].symbolize_keys
+rescue
+  nil
+end
