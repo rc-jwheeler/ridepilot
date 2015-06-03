9956ba0e7a792a9ce4af14f0750179f7cab4d068
Xudong Liu <xudong.camsys@gmail.com>
2015-06-03 10:48:31 -0400

    using postgis as adapter to access DB

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,28 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module Ridepilot
+  class ActiveRecordOverrideRailtie < Rails::Railtie
+    initializer "active_record.initialize_database.override" do |app|
+
+      ActiveSupport.on_load(:active_record) do
+        if url = ENV['DATABASE_URL']
+          ActiveRecord::Base.connection_pool.disconnect!
+          parsed_url = URI.parse(url)
+          config =  {
+            adapter:             'postgis',
+            host:                parsed_url.host,
+            encoding:            'unicode',
+            database:            parsed_url.path.split("/")[-1],
+            port:                parsed_url.port,
+            username:            parsed_url.user,
+            password:            parsed_url.password
+          }
+          establish_connection(config)
+        end
+      end
+    end
+  end
+  
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
