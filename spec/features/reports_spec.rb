78573fcfce14b9e37e5fc413ac68408cb5275387
Alex Bromley <evilbikes79@gmail.com>
2015-07-08 15:37:56 -0400

    update tests with host for links [#98320888]

--- spec/features/reports_spec.rb
+++ spec/features/reports_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe "Reports" do
       visit new_user_session_path
       fill_in 'user_email', :with => user.email
       fill_in 'Password', :with => 'password#1'
-      click_button 'Log in'
+      click_button 'Log In'
     end
     
     describe "GET /customer/:id" do
