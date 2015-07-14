86e5341a46fe7efb13bdfebaf576ec73d24baedc
Xudong Liu <xudong.camsys@gmail.com>
2015-07-14 14:04:57 -0400

    remove ride_connection admin configuration; cleanup seed file to seed UTA as first provider

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,34 @@
 ActiveRecord::Base.transaction do
   puts "Seeding..."
 
-  puts "Creating initial mobilities..."
-  ["Scooter", "Wheelchair", "Wheelchair - Oversized", "Wheelchair - Can Transfer", "Unknown", "Ambulatory"].each do |name|
-    Mobility.find_or_create_by!(:name => name)
+  puts "Creating first provider and system admin..."
+  if !Provider.first.present?
+    puts "Creating first Provider UTA..."
+    provider = Provider.new(:name => 'UTA', :dispatch => true)
+    provider.logo = File.open(Rails.root.join("public", "uta_logo.png"))
+    provider.save!
+  
+    puts "Creating first User..."
+    password = Rails.application.secrets.system_admin_password
+    user = User.create!(
+      :email => Rails.application.secrets.system_admin_email,
+      :password => password,
+      :password_confirmation => password,
+      :current_provider => provider
+    )
+  
+    puts "Setting first user up as a super admin..."
+    Role.create!(
+      :user => user,
+      :provider => provider, 
+      :level => 200
+    )
   end
 
-  puts "Seeding translations"
+  puts "Creating lookup tables..."
+  Rake::Task["ridepilot:seed_lookup_tables"].invoke
 
+  puts "Seeding translations"
   Rake::Task["ridepilot:load_locales"].invoke
 
   puts "Done seeding"
