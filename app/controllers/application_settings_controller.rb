183eef8b52ba6bf24cf1e7e9509142dd1b46224e
Chris Bloom <chrisbloom7@gmail.com>
2015-07-20 18:01:44 -0400

    Add controller for managing application settings [#98938970][#98938982]

---
+++ app/controllers/application_settings_controller.rb
@@ -0,0 +1,29 @@
+class ApplicationSettingsController < ApplicationController
+  authorize_resource
+  
+  def index
+  end
+
+  def edit
+    @settings = ApplicationSetting.all
+  end
+
+  def update
+    respond_to do |format|
+      if ApplicationSetting.update_settings(application_setting_params)
+        format.html { redirect_to application_settings_path, notice: 'Application settings were successfully updated.' }
+        format.json { head :no_content }
+      else
+        @settings = ApplicationSetting.all.merge(params[:application_setting])
+        format.html { render action: :edit }
+        format.json { render json: ApplicationSetting.all, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def application_setting_params
+    params.require(:application_setting).permit!
+  end
+end
