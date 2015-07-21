1e6e95959d898d7020ecf64e0fd42b6942d9bd2b
Chris Bloom <chrisbloom7@gmail.com>
2015-07-21 13:59:07 -0400

    Backfilling user spec [#98939210]

--- spec/models/driver_spec.rb
+++ spec/models/driver_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Driver, type: :model do
     driver_2 = build :driver, user: driver_1.user
     expect(driver_2.valid?).to be_falsey
     expect(driver_2.errors.keys).to include :user_id
-    
+
     driver_2.user = create :user
     expect(driver_2.valid?).to be_truthy
   end
@@ -16,29 +16,29 @@ RSpec.describe Driver, type: :model do
     driver_2 = build :driver, name: driver_1.name, provider: driver_1.provider
     expect(driver_2.valid?).to be_falsey
     expect(driver_2.errors.keys).to include :name
-    
+
     driver_2.provider = create :provider
     expect(driver_2.valid?).to be_truthy
   end
-  
+
   it "must have a name at least 2 characters in length" do
     driver = build :driver, name: "M"
     expect(driver.valid?).to be_falsey
     expect(driver.errors.keys).to include :name
-    
+
     driver.name = "Mo"
     expect(driver.valid?).to be_truthy
   end
-  
+
   it "must have a valid email when specified" do
     driver = build :driver, email: "m@m"
     expect(driver.valid?).to be_falsey
     expect(driver.errors.keys).to include :email
-    
+
     driver.email = "m@m.m"
     expect(driver.valid?).to be_truthy
   end
-  
+
   it "can find drivers for a given provider" do
     driver_1 = create :driver
     driver_2 = create :driver
@@ -46,7 +46,7 @@ RSpec.describe Driver, type: :model do
     expect(drivers).to include driver_1
     expect(drivers).not_to include driver_2
   end
-  
+
   it "can find drivers who are not assigned to a device pool" do
     provider = create :provider
     driver_1 = create :driver, provider: provider
