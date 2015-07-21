fa3f1082e5b97c75806b43c1b6851ac5a924cf4d
Xudong Liu <xudong.camsys@gmail.com>
2015-07-21 14:17:53 -0400

    trip filters UI; added trip_filter service class to handle filtering logic

---
+++ app/services/trip_filter.rb
@@ -0,0 +1,61 @@
+class TripFilter 
+
+  attr_reader :trips, :filters
+
+  def initialize(trips, filters = {})
+    @trips = trips
+    @filters = filters
+  end
+
+  def filter!
+    filter_by_pickup_time!
+    filter_by_vehicle!
+    filter_by_status!
+
+    @filters.each do |k, v|
+      next if [:start, :end, :vehicle_id, :status_id].index(k) # has been processed above
+
+      @trips = @trips.where("#{k}": v) if !v.blank?
+    end
+
+    @trips
+  end
+
+  private 
+
+  def filter_by_pickup_time!
+    if @filters[:end].present? && @filters[:start].present?
+      t_start = Time.at(@filters[:start].to_i).to_date.in_time_zone.utc
+      t_end   = Time.at(@filters[:end].to_i).to_date.in_time_zone.utc
+    else
+      time    = Time.now
+      t_start = time.beginning_of_week.to_date.in_time_zone.utc
+      t_end   = t_start + 6.days
+    end
+
+    @trips = @trips.
+      where("pickup_time >= '#{t_start.strftime "%Y-%m-%d %H:%M:%S"}'").
+      where("pickup_time <= '#{t_end.strftime "%Y-%m-%d %H:%M:%S"}'").order(:pickup_time)
+  end
+
+  def filter_by_vehicle!
+    if @filters[:vehicle_id].present?  
+      if @filters[:vehicle_id].to_i == -1
+        @trips = @trips.where(cab: true)
+      else
+        @trips = @trips.where(vehicle_id: @filters[:vehicle_id]) 
+      end
+    end
+  end
+
+  def filter_by_status!
+    if @filters[:status_id].present?  
+      if @filters[:status_id].to_i == 1
+        @trips = @trips.where.not(run_id: nil)
+      else
+        @trips = @trips.where(run_id: nil)
+      end
+    end
+  end
+
+end
