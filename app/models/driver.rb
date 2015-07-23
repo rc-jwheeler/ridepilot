072582b8f2364e751d7c3fc779b5a6af1975f8b5
Chris Bloom <chrisbloom7@gmail.com>
2015-07-23 11:47:23 -0400

    Allow driver to accept nested attributes for driver histories [#98939212]

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -9,6 +9,8 @@ class Driver < ActiveRecord::Base
 
   has_many :operating_hours, class_name: :OperatingHours, dependent: :destroy
   has_many :driver_histories, dependent: :destroy, inverse_of: :driver
+
+  accepts_nested_attributes_for :driver_histories, allow_destroy: true, reject_if: proc { |attributes| attributes['event'].blank? }
   
   validates :user_id, uniqueness: {allow_nil: true}
   validates :name, uniqueness: {scope: :provider_id}, length: {minimum: 2}
