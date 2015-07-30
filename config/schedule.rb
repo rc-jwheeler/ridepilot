83146232d261190f7050016b1715d1ddccc91760
Chris Bloom <chrisbloom7@gmail.com>
2015-07-30 11:15:51 -0400

    Annotating config/schedule.rb [#98939234]

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,8 @@
+# Cron job scheduler. Integrates with Capistrano, or update your custom 
+# deployments to manually execute whenever. See `bundle exec whenever --help` 
+# for details
 
-every 1.day do
+# Schedule repeating trips
+every 1.day, :at => '12:00 am' do
   runner "RepeatingTrip.create_trips"       
 end
