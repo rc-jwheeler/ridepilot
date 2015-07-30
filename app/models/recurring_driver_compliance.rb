3f9595d524bf204e151071e4d4625b4737dd1725
Chris Bloom <chrisbloom7@gmail.com>
2015-07-30 19:48:24 -0400

    Adding incremental functionality to recurring driver compliance model [#98939234]

--- app/models/recurring_driver_compliance.rb
+++ app/models/recurring_driver_compliance.rb
@@ -2,10 +2,12 @@ class RecurringDriverCompliance < ActiveRecord::Base
   RECURRENCE_SCHEDULES = [:days, :weeks, :months, :years].freeze
   FUTURE_START_RULES = [:immediately, :on_schedule, :time_span].freeze
   
-  belongs_to :provider
+  after_update :update_children
+
+  belongs_to :provider, inverse_of: :recurring_driver_compliances
   
   has_many :drivers, through: :provider
-  has_many :driver_compliances, :dependent => :nullify
+  has_many :driver_compliances, :dependent => :nullify, inverse_of: :recurring_driver_compliance
   
   scope :default_order, -> { order("start_date DESC") }
   
@@ -18,8 +20,33 @@ class RecurringDriverCompliance < ActiveRecord::Base
   validates :future_start_frequency, numericality: { only_integer: true, greater_than: 0, if: :future_start_rule_is_time_span? }
   validates :compliance_date_based_scheduling, inclusion: { in: [true, false] }
   validates_date :start_date, on_or_after: -> { Date.current }
+  validate :limit_updates_on_recurrences_with_children, on: :update
+  
+  def destroy_with_incomplete_children!
+    RecurringDriverCompliance.transaction do
+      child_ids = driver_compliances.incomplete.pluck(:id)
+      self.destroy
+      DriverCompliance.destroy_all(id: child_ids)
+    end
+  end
+  
+  private
   
   def future_start_rule_is_time_span?
-    future_start_rule.to_sym == :time_span
+    future_start_rule.present? && future_start_rule.to_sym == :time_span
+  end
+
+  # Only allow updating the event_name and event_notes fields if the record is
+  # associated with any DriverCompliance records
+  def limit_updates_on_recurrences_with_children
+    if driver_compliances.any?
+      changed_attributes.except(:event_name, :event_notes).keys.each do |key|
+        errors.add(key, "cannot be modified once events have been generated")
+      end
+    end
+  end
+  
+  def update_children
+    driver_compliances.update_all event: event_name, notes: event_notes
   end
 end
--- spec/models/recurring_driver_compliance_spec.rb
+++ spec/models/recurring_driver_compliance_spec.rb
@@ -53,7 +53,7 @@ RSpec.describe RecurringDriverCompliance, type: :model do
   end
   
   it "requires a future start rule of either 'immediately', 'on_schedule' (i.e. based on start date), or 'time_span'" do
-    recurrence = build :recurring_driver_compliance, future_start_rule: nil
+    recurrence = build :recurring_driver_compliance, future_start_rule: nil, future_start_schedule: 'days', future_start_frequency: 1
     expect(recurrence.valid?).to be_falsey
     expect(recurrence.errors.keys).to include :future_start_rule
     
@@ -109,31 +109,77 @@ RSpec.describe RecurringDriverCompliance, type: :model do
     end
   end
   
-  it "schedules new events on schedule based on the due date for 6 months out when due date scheduling is preferred"
-  
-  it "schedules 1 new event on schedule based on the compliance date only after the previous event is complete when compliance date scheduling is preferred"
-  
   it "does not automatically generate child compliance events on creation (allowing for a period of time to modify the new event)" do
     expect {
       create :recurring_driver_compliance
     }.not_to change(DriverCompliance, :count)
   end
+  
+  describe "updating" do
+    it "only allows the event name and event notes fields to be modified once it has spawned children" do
+      recurrence = create :recurring_driver_compliance, event_name: "My Event", event_notes: nil
+      create :driver_compliance, recurring_driver_compliance: recurrence
 
-  it "only generates child compliance events for drivers of its own provider"
-  
-  it "knows how to apply events to drivers created after it was defined"
-  
-  it "does not allow scheduling attributes to be modified once it has generated children"
-  
-  it "pushes changes to event name and event notes fields to children"
-  
-  it "nullifies the association on children when destroyed, by default"
-  
-  it "can optionally delete incomplete children when destroyed, but will still nullify complete children"
+      recurrence.start_date = Date.tomorrow
+      expect(recurrence.valid?).to be_falsey
+      expect(recurrence.errors.keys).to include :start_date
 
-  it "can rollback all changes (including its own deletion) when an error occurs while deleting with the optional flag to delete incomplete children"
+      recurrence.reload
+      recurrence.event_name = "My New Event"
+      recurrence.event_notes = "My Notes"
+      expect(recurrence.valid?).to be_truthy
+    end
+  
+    it "pushes changes to event name and event notes fields to children" do
+      recurrence = create :recurring_driver_compliance, event_name: "My Event", event_notes: nil
+      driver_compliance = create :driver_compliance, recurring_driver_compliance: recurrence
+      
+      expect {
+        recurrence.update_attributes event_name: "My Update Event Name", event_notes: "My Updated Event Notes"
+      }.to change { [driver_compliance.reload.event, driver_compliance.reload.notes] }.to(["My Update Event Name", "My Updated Event Notes"])
+    end
+  end
+  
+  describe "destroying" do
+    it "nullifies the association on children when destroyed, by default" do
+      recurrence = create :recurring_driver_compliance, event_name: "My Event", event_notes: nil
+      driver_compliance = create :driver_compliance, recurring_driver_compliance: recurrence
+      
+      expect {
+        recurrence.destroy
+      }.to change(RecurringDriverCompliance, :count).by(-1)
+      expect(driver_compliance.reload.recurring_driver_compliance).to be_nil
+    end
+  
+    it "can optionally delete incomplete children when destroyed, but will still nullify complete children" do
+      recurrence = create :recurring_driver_compliance, event_name: "My Event", event_notes: nil
+      driver_compliance_1 = create :driver_compliance, compliance_date: Date.current, recurring_driver_compliance: recurrence
+      driver_compliance_2 = create :driver_compliance, recurring_driver_compliance: recurrence
+      driver_compliance_3 = create :driver_compliance, :recurring
+      
+      expect {
+        recurrence.destroy_with_incomplete_children!
+      }.to change(RecurringDriverCompliance, :count).by(-1)
+      expect(driver_compliance_1.reload.recurring_driver_compliance).to be_nil
+      expect(DriverCompliance.all).to include driver_compliance_1
+      expect(DriverCompliance.all).not_to include driver_compliance_2
+      expect(DriverCompliance.all).to include driver_compliance_3
+    end
+  end
+  
+  describe "event generation" do
+    it "only generates child compliance events for drivers of its own provider"
+  
+    it "sets the name and notes of children to the event name and event notes field, respectively"
 
-  it "can calculate the first event date based on the start schedule for a given driver"
+    it "schedules new events on schedule based on the due date for 6 months out when due date scheduling is preferred"
+  
+    it "schedules 1 new event on schedule based on the compliance date only after the previous event is complete when compliance date scheduling is preferred"
+  
+    it "knows how to apply events to drivers created after it was defined"
+  
+    it "can calculate the first event date based on the start schedule for a given driver"
 
-  it "can calculate the next event date based on the recurrence schedule for a given driver"
+    it "can calculate the next event date based on the recurrence schedule for a given driver"
+  end
 end
