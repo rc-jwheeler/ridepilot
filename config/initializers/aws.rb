e2e6b3a4651ec1615934e14fdf742210b16e25b9
Xudong Liu <xudong.camsys@gmail.com>
2015-08-04 14:30:51 -0400

    add ENV var check when initializing AWS

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,7 +1,10 @@
-Aws.config.update(
-  credentials: Aws::Credentials.new(
-    ENV['AWS_KEY_ID'],
-    ENV['AWS_ACCESS_KEY'])
-)
+if ENV['AWS_REGION'] && ENV['AWS_KEY_ID'] && ENV['AWS_ACCESS_KEY']
+  Aws.config.update(
+    region: 
+    credentials: Aws::Credentials.new(
+      ENV['AWS_KEY_ID'],
+      ENV['AWS_ACCESS_KEY'])
+  )
+end
 
-S3_BUCKET =  Aws::S3::Resource.new.bucket(ENV['AWS_BUCKET'])
+S3_BUCKET =  Aws::S3::Resource.new.bucket(ENV['AWS_BUCKET']) if ENV['AWS_BUCKET']
