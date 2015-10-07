1f592ed0a107d0751fa15f6805f886fcf8ebf072
Chris Bloom <chrisbloom7@gmail.com>
2015-10-07 12:38:44 -0400

    Make default_driver optional on vehicle model [finishes#105010316]

--- spec/factories/vehicles.rb
+++ spec/factories/vehicles.rb
@@ -2,7 +2,6 @@ FactoryGirl.define do
   factory :vehicle do
     name { Faker::Lorem.words(2).join(' ') }
     provider
-    association :default_driver, factory: :driver
     seating_capacity 10
     mobility_device_accommodations 2
   end
