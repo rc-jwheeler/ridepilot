8187f718bad2dbc92707403371553656146c9750
Xudong Liu <xudong.camsys@gmail.com>
2015-10-07 18:55:23 -0400

    Another typo

--- app/controllers/api/v1/trip_purposes_controller.rb
+++ app/controllers/api/v1/trip_purposes_controller.rb
@@ -1,12 +1,12 @@
 class API::V1::TripPurposesController < API::ApiController
   
   def index
-    Provider = Provider.find_by_id(params[:provider_id])
+    provider = Provider.find_by_id(params[:provider_id])
 
-    if !Provider
+    if !provider
       error(:unprocessable_entity, TranslationEngine.translate_text(:provider_not_exist))
     else
-      render json: { trip_purposes: TripPurpose.by_provider(Provider).map(&:as_api_json) }
+      render json: { trip_purposes: TripPurpose.by_provider(provider).map(&:as_api_json) }
     end
 
   end
