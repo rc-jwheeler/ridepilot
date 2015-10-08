e857c42fdadf4a25381e2ae604f5c4cfee4057df
Xudong Liu <xudong.camsys@gmail.com>
2015-10-08 14:52:12 -0400

    Update token generation

--- spec/factories/booking_users.rb
+++ spec/factories/booking_users.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :booking_user do
     user 
-    token { Faker::Lorem.characters(16) }
     url { Faker::Internet.url }
+    token SecureRandom.uuid
   end
 
 end
--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -5,5 +5,6 @@ FactoryGirl.define do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     provider
+    token SecureRandom.uuid
   end
 end
