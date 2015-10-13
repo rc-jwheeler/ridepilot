03ff41c13c6b925bb23ebabcc5437e3514dc1901
Xudong Liu <xudong.camsys@gmail.com>
2015-10-13 18:10:51 -0400

    Use current provider for new trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -232,12 +232,7 @@ class TripsController < ApplicationController
   end
 
   def create
-    if params[:trip][:customer_id] && customer = Customer.find_by_id(params[:trip][:customer_id])
-      #authorize! :read, customer
-      params[:trip][:provider_id] = customer.provider.id if customer.provider.present?
-    else
-      params[:trip][:customer_id] = ""
-    end    
+    params[:trip][:provider_id] = current_provider_id   
     handle_trip_params params[:trip]
     @trip = Trip.new(trip_params)
     authorize! :manage, @trip
