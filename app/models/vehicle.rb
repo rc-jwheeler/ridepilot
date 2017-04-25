6b551dc9cade2a6a787d4f223781603b7778f36c
Xudong Liu <xudong.camsys@gmail.com>
2017-04-25 11:00:11 -0400

    Make vehicle name, VIN, license plate unique within a provider(G.1)

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -25,8 +25,9 @@ class Vehicle < ActiveRecord::Base
   has_many :vehicle_maintenance_compliances, dependent: :delete_all, inverse_of: :vehicle
 
   validates :provider, presence: true
-  validates :name, presence: true
-  validates :vin, length: {is: 17, allow_nil: true, allow_blank: true},
+  validates :name, presence: true, uniqueness: { scope: :provider, message: "should be unique" }
+  validates :license_plate, uniqueness: { scope: :provider, message: "should be unique" }, allow_nil: true, allow_blank: true
+  validates :vin, uniqueness: { scope: :provider, message: "should be unique" }, length: {is: 17, allow_nil: true, allow_blank: true},
     format: {with: /\A[^ioq]*\z/i, allow_nil: true}
   validates_date :registration_expiration_date, allow_blank: true
   validates :seating_capacity, numericality: { only_integer: true, greater_than: 0 }
