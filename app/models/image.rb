60e11dc404367018968f78855ead7c42a9a50425
Xudong Liu <xudong.camsys@gmail.com>
2017-05-02 12:15:35 -0400

    Improve error message for image uploading [RPII-69]

--- app/models/image.rb
+++ app/models/image.rb
@@ -8,6 +8,6 @@ class Image < ActiveRecord::Base
   validates_attachment_presence :image
   validates_attachment_content_type :image, content_type: [
       "image/jpeg", "image/gif", "image/png" # image files (.png, .gif, .jpg)
-    ], if: lambda { |d| d.image.present? }
+    ], message: "should be JPEG, PNG, or GIF", if: lambda { |d| d.image.present? }
   validates_attachment_size :image, :in => 1..2.gigabytes, if: lambda { |d| d.image.present? }
 end
