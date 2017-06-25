d0441e9f6af54bae74f3112fdef332c1307eddde
Xudong Liu <xudong.camsys@gmail.com>
2017-06-25 10:36:45 -0400

    Fix factory

--- spec/factories/vehicle_compliances.rb
+++ spec/factories/vehicle_compliances.rb
@@ -1,12 +1,13 @@
 FactoryGirl.define do
   factory :vehicle_compliance do
-    vehicle nil
-event "MyString"
-notes "MyText"
-due_date "2017-06-24 00:00:00"
-compliance_date "2017-06-24 00:00:00"
-vehicle_requirement_template nil
-legal false
+    vehicle 
+    vehicle_requirement_template
+    event { Faker::Lorem.words(2).join(' ') }
+    due_date { Date.current.tomorrow }
+    
+    trait :complete do
+      compliance_date { Date.current }
+    end
   end
 
 end
