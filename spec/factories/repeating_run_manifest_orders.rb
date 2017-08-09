d866496dff7907c8eed3c1d04c9308903d645aba
Xudong Liu <xudong.camsys@gmail.com>
2017-08-09 18:56:12 -0400

    Fix factory

--- spec/factories/repeating_run_manifest_orders.rb
+++ spec/factories/repeating_run_manifest_orders.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :repeating_run_manifest_order do
-    repeating_run nil
-wday 1
-manifest_order "MyText"
+    repeating_run 
+    wday 1
+    manifest_order []
   end
 
 end
