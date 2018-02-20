c58d8efea519033fb2ee862b7a240e438daf7db0
Xudong Liu <xudong.camsys@gmail.com>
2018-02-20 16:59:23 -0500

    add migration rails version number

--- db/migrate/20180123045647_change_lookup_table_column_name.rb
+++ db/migrate/20180123045647_change_lookup_table_column_name.rb
@@ -1,4 +1,4 @@
-class ChangeLookupTableColumnName < ActiveRecord::Migration
+class ChangeLookupTableColumnName < ActiveRecord::Migration[5.1]
   def change
     rename_column :lookup_tables, :model_name, :model_name_str
     rename_column :provider_lookup_tables, :model_name, :model_name_str
