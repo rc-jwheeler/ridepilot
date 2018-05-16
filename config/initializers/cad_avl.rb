f457d12a6d79a7f3af8ab64aac921a6b8cd95b65
Xudong Liu <xudong.camsys@gmail.com>
2018-05-16 18:02:55 -0400

    add manifest change check frequency setting

--- config/initializers/cad_avl.rb
+++ config/initializers/cad_avl.rb
@@ -1,5 +1,6 @@
 if !Rails.env.test?
   ApplicationSetting['cad_avl.gps_interval_seconds'] = 10
   ApplicationSetting['cad_avl.cad_refresh_interval_seconds'] = 30
+  ApplicationSetting['cad_avl.manifest_change_check_interval_seconds'] = 5
   ApplicationSetting['cad_avl.data_storage_months'] = 1
 end
