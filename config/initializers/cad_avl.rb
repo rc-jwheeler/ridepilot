8acae2b2dbc5767a3a2cbf6424d3c9ea31b5c63c
Xudong Liu <xudong.camsys@gmail.com>
2018-11-03 00:24:52 -0400

    check settings table exists

--- config/initializers/cad_avl.rb
+++ config/initializers/cad_avl.rb
@@ -1,4 +1,4 @@
-if !Rails.env.test?
+if !Rails.env.test? && ActiveRecord::Base.connection.table_exists?('settings')
   ApplicationSetting['cad_avl.gps_interval_seconds'] = 10
   ApplicationSetting['cad_avl.cad_refresh_interval_seconds'] = 30
   ApplicationSetting['cad_avl.data_storage_months'] = 1
